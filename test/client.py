c2d0c98307019496a46224d9dff78717e82afecd
Yash Parakh <yash.parakh111@gmail.com>
2019-03-15 11:02:43 -0500

    Able to send images

--- test/client.py
+++ test/client.py
@@ -1,9 +1,27 @@
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("10.148.58.0", 1234))    # PC static IP
-#s.connect((socket.gethostname(), 1234))
+# read test image to send over network
+with open("test_image.png", "rb") as image:
+    f = image.read()
+    #b = bytearray(f)
 
-msg = s.recv(1024)
-print(msg.decode("utf-8"))
+HOST = "127.0.0.1"
+PORT = 63222
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(f)  # sends a bytes object
+    data = s.recv(600*530*3)
+
+print(len(data))
+print('Recieved', (data))
+
+# import socket
+# 
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# s.connect(("10.153.85.238", 1234))    # PC static IP
+# #s.connect((socket.gethostname(), 1234))
+# 
+# msg = s.recv(1024)
+# print(msg.decode("utf-8"))
  
--- test/server.py
+++ test/server.py
@@ -1,13 +1,28 @@
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(("", 1234))
-s.listen(1)
+HOST = "127.0.0.1"
+PORT = 63222
 
-while True:
-    clientsocket, address = s.accept()
-    print(f"Connection from {address} has been established!")
-    clientsocket.send(bytes("Welcome to the server!", "utf-8"))
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True: # keep listening for client
+            data = conn.recv(600*530*3)  # listens for a bytes object
+            if not data:
+                break
+            conn.sendall(data)
 
-    clientsocket.close()
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# s.bind(("127.0.0.1", 1234))
+# s.listen(1)
+# 
+# while True:
+#     clientsocket, address = s.accept()
+#     print(f"Connection from {address} has been established!")
+#     clientsocket.send(bytes("Welcome to the server!", "utf-8"))
+# 
+#     clientsocket.close()
 
