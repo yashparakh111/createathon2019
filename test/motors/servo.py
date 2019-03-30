638d1625635c7fa0bdc5936bb7056077e20c1a5d
Yash Parakh <yash.parakh111@gmail.com>
2019-03-30 00:01:38 -0500

    Initial servo code

---
+++ test/motors/servo.py
@@ -0,0 +1,28 @@
+import RPi.GPIO as GPIO
+import time
+
+RIGHT_SERVO_2_PIN = 16
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(RIGHT_SERVO_2_PIN, GPIO.OUT)
+
+p = GPIO.PWM(RIGHT_SERVO_2_PIN, 50)
+p.start(2.5)
+
+try:
+    while True:
+        p.ChangeDutyCycle(5)
+        time.sleep(0.5)
+        p.ChangeDutyCycle(7.5)
+        time.sleep(0.5)
+        p.ChangeDutyCycle(10)
+        time.sleep(0.5)
+        p.ChangeDutyCycle(12.5)
+        time.sleep(0.5)
+        p.ChangeDutyCycle(10)
+        time.sleep(0.5)
+        p.ChangeDutyCycle(7.5)
+        time.sleep(0.5)
+        p.ChangeDutyCycle(5)
+        time.sleep(0.5)
+        p.ChangeDutyCycle(2.5)
+        time.sleep(0.5)
